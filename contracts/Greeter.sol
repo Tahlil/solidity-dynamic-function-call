1bb8ca78e4abd927f0642220448740c5ba4752ae
tahlil <bsse0803@iit.du.ac.bd>
2022-07-06 08:12:28 +0600

    Create function to set two values

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -5,15 +5,22 @@ import "hardhat/console.sol";
 
 contract Greeter {
     uint private value;
+    uint private value2;
 
     constructor(uint _value) {
         console.log("Deploying a Greeter with value:", _value);
         value = _value;
+        value2 = _value;
     }
 
     
     function set(uint n) external{
         value = n;
+    }
+
+    function setTwoValue(uint n, uint n2) external{
+        value = n;
+        value2 = n2;
     }
     
     function get() external view returns(uint) {
